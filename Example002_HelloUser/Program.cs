b217f874143b22c790c7a708c6ff7d69849c70c8
YuriSol <y.salokhin@mail.ru>
2022-06-17 08:16:53 +0300

    add Example002 HelloUser

---
+++ Example002_HelloUser/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Введите Ваше имя: "); //Выводим приглашение к вводу имени пользователя в консоль
+string  Username = Console.ReadLine(); //Создаем переменную Username типа string и считываем 
+//в неё с помощью Console.ReadLine() имя которое введет пользователь
+
+Console.WriteLine("Привет, " + Username); //С помощю комманды Console.WriteLine выводим в консоль приветствие
+//Console.WriteLine();  //и имя введенное пользователем
