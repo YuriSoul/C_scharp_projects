dad3bc265923f342f15cebf96d9e3df4312dde6a
YuriSol <y.salokhin@mail.ru>
2022-06-24 10:33:55 +0300

    add code

--- Example010_MethodArray/Program.cs
+++ Example010_MethodArray/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Функция поиска заданного числа в массиве
+
+//создаем массив
+int[] array = { 11, 21, 35, 4, 5, 62, 7, 89, 9 };
+
+int n = array.Length; //получаем размер массива
+
+int find = 62; //задаем число для поиска 
+
+int index = 0;
+
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break; //если условие выполняется то происходит прерывание цикла
+    } 
+    index++;
+}
+
+
+
